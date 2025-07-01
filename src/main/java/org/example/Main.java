d98d06102fdc2b9982ca11ed062edd3c0e0709f6
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-07-01 16:34:29 +0800

    feat: add solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,93 @@ package org.example;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args) {
+    public static String calculateLetterGrade (int score){
+        String grade = "";
+        if (score >= 90 && score <= 100) grade = "A";
+        else if (score >= 80) grade = "B";
+        else if (score >= 70) grade = "C";
+        else if (score >= 60) grade = "D";
+        else grade = "F";
+        return grade;
+    }
 
+    public static float calculateAverage (int[] scores){
+        float sum = 0, average = 0;
+        for (int score: scores){
+            sum += score;
+        }
+        average = sum / scores.length;
+        return average;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] gradeCounts = new int[5];
+
+        System.out.print("Enter number of students: ");
+        int num = Integer.parseInt(scanner.nextLine());
+        String[] names = new String[num];
+        int[] scores = new int[num];
+        String[] topStudents = new String[num];
+        int topStudentsCount = 0;
+        int highestScore = -1;
+
+        for (int i = 1; i <= num; i++ ){
+            System.out.printf("%nEnter name of student %d: ", i);
+            String name = scanner.nextLine();
+            names[i-1] = name;
+
+            System.out.printf("Enter score for %s: ", name);
+            int score = Integer.parseInt(scanner.nextLine());
+            scores[i-1] = score;
+
+            if (score > highestScore){
+                highestScore = score;
+                topStudentsCount = 0;
+                topStudents[topStudentsCount] = name;
+                topStudentsCount++;
+            } else if (score == highestScore) {
+                topStudents[topStudentsCount] = name;
+                topStudentsCount++;
+            }
+
+            String letterGrade = (calculateLetterGrade(score));
+            switch (letterGrade){
+                case "A":
+                    gradeCounts[0]++;
+                    break;
+                case "B":
+                    gradeCounts[1]++;
+                    break;
+                case "C":
+                    gradeCounts[2]++;
+                    break;
+                case "D":
+                    gradeCounts[3]++;
+                    break;
+                case "F":
+                    gradeCounts[4]++;
+                    break;
+                default:
+                    break;
+            }
+            System.out.printf("%s got grade: %s %n", name, letterGrade);
+        }
+
+        System.out.printf("%n----- Class Summary -----%n");
+        System.out.printf("Average Score: %.2f %n", calculateAverage(scores));
+
+        System.out.printf("Grade Counts: ");
+        System.out.printf("A:%d ", gradeCounts[0]);
+        System.out.printf("B:%d ", gradeCounts[1]);
+        System.out.printf("C:%d ", gradeCounts[2]);
+        System.out.printf("D:%d ", gradeCounts[3]);
+        System.out.printf("F:%d %n", gradeCounts[4]);
+        System.out.printf("Top Student(s): ");
+        for (int i = 0; i < topStudentsCount; i++){
+            System.out.printf(topStudents[i]);
+            if (i < topStudentsCount - 1) System.out.print(", ");
+        }
+        System.out.printf(" (%d) %n", highestScore);
     }
 }
